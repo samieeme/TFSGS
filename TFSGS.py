8afd9a7ccafcec9657347f678f06cfa84728e30e
Mehdi Samiee <samieeme@msu.edu>
2019-05-29 19:24:19 -0400

    TFSGS
    Changed input-time-step

--- TFSGS.py
+++ TFSGS.py
@@ -8,8 +8,12 @@ import numpy as np
 from functions import remain, deriv_x, deriv_y, deriv_z, div, Reduce_period
 from Outputs import Output_Corr
 
-Rs = input("filter width:  ")
+#del( time, Rs, filename, fileout, corr_smg, corr_fsgs)
+
 time = input("Time:  ")
+Rs = input("filter width:  ")
+
+
 filename="C:/Users/samieeme.CMSE020/Desktop/New folder/semesters/PHD MSU/Semester 9/research/DNS-data/Nektar/20"
 #filename="C:/Users/samieeme.CMSE020/Desktop/New folder/semesters/PHD MSU/Semester 9/research/DNS-data/t-1000/DNS_"+ Rs
 #filename="C:/Users/samieeme.CMSE020/Desktop/New folder/semesters/PHD MSU/Semester 9/research/DNS-data/Nektar/20/"
@@ -17,16 +21,16 @@ fileout = 'C:/Users/samieeme.CMSE020/Desktop/New folder/semesters/PHD MSU/Semest
 
 
 
-solver = Output_Corr(filename,Rs)
+solver = Output_Corr(filename,Rs,time)
 
 corr_smg = solver.SMG_Model()
-np.savetxt(fileout+'output-time-'+str(time)+'-R'+str(Rs)+'-SMG.csv', corr_smg)
+np.savetxt(fileout+'output-time-'+time+'-R'+Rs+'-SMG.csv', corr_smg)
 
 
-nu = 0.000185
-alpha1 = 0.70
+nu = 1.0/1600
+alpha1 = 0.80
 corr_fsgs = solver.FSGS_Model(alpha1,nu)
-np.savetxt(fileout+'output-time-'+str(time)+'-R'+str(Rs)+'-FSGS-alpha-'+str(int(alpha1*100))+'.csv', corr_fsgs)
+np.savetxt(fileout+'output-time-'+time+'-R'+Rs+'-FSGS-alpha-'+str(int(alpha1*100))+'.csv', corr_fsgs)
 
 
 #alpha2 = 0.75
